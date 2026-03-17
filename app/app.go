eec44b4cf1c96a02e43b784242fbfa3f24a25672
Leyuan Pan <leyuanpan@gmail.com>
2026-03-17 01:22:15 -0700

    Minor fix

--- app/app.go
+++ app/app.go
@@ -2,12 +2,43 @@ package app
 
 import (
 	"context"
+	"encoding/base64"
+	"log"
+	"os"
+	"strings"
+	"time"
 
+	"github.com/libp2p/go-libp2p"
 	coreconnmgr "github.com/libp2p/go-libp2p/core/connmgr"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/pnet"
+	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
+	"github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
+	quic "github.com/libp2p/go-libp2p/p2p/transport/quic"
 	"github.com/pancpp/peanut-relay/conf"
 )
 
+var (
+	gHost      host.Host
+	gConnGater coreconnmgr.ConnectionGater
+)
+
 func Init(ctx context.Context) error {
+	// init connection gater
+	if err := initConnGater(); err != nil {
+		return err
+	}
+
+	// init relay
+	if err := initRelay(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func initConnGater() error {
 	// create connection gater
 	var connGater coreconnmgr.ConnectionGater
 	if conf.GetBool("p2p.enable_gater") {
@@ -18,10 +49,101 @@ func Init(ctx context.Context) error {
 		}
 	}
 
-	// init relay
-	if err := initRelay(ctx, connGater); err != nil {
+	gConnGater = connGater
+
+	return nil
+}
+
+func initRelay(ctx context.Context) error {
+	// p2p opts
+	var opts []libp2p.Option
+
+	// private key
+	privateKeyPath := conf.GetString("p2p.private_key_path")
+	privateKeyB64, err := os.ReadFile(privateKeyPath)
+	if err != nil {
+		log.Printf("reading private key err: %v, path: %s", err, privateKeyPath)
 		return err
 	}
+	privateKeyBytes, err := base64.StdEncoding.DecodeString(strings.TrimSpace(string(privateKeyB64)))
+	if err != nil {
+		log.Printf("base64 unmarshal err: %v, string: %s", err, string(privateKeyB64))
+		return err
+	}
+	privateKey, err := crypto.UnmarshalPrivateKey(privateKeyBytes)
+	if err != nil {
+		log.Printf("invalid private key, err: %v, string: %s", err, string(privateKeyBytes))
+		return err
+	}
+	opts = append(opts, libp2p.Identity(privateKey))
+
+	// listen addresses
+	opts = append(opts, libp2p.Transport((quic.NewTransport)))
+	listenAddrs := conf.GetStringSlice("p2p.listen_multiaddrs")
+	if len(listenAddrs) > 0 {
+		opts = append(opts, libp2p.ListenAddrStrings(listenAddrs...))
+	}
+
+	// relay
+	relayRes := relay.DefaultResources()
+	relayRes.Limit = nil
+	relayRes.ReservationTTL = time.Duration(conf.GetInt("relay.reservation_ttl")) * time.Second
+	relayOpts := []relay.Option{relay.WithResources(relayRes)}
+
+	// add connection gater
+	if gConnGater != nil {
+		opts = append(opts, libp2p.ConnectionGater(gConnGater))
+	}
+
+	opts = append(opts,
+		libp2p.ForceReachabilityPublic(),
+		libp2p.EnableRelayService(relayOpts...),
+		libp2p.DisableRelay(),
+	)
+
+	// NAT service
+	opts = append(opts, libp2p.EnableNATService())
+
+	// pnet psk
+	pskPath := conf.GetString("p2p.pnet_psk_path")
+	if pskPath != "" {
+		pskFile, err := os.Open(pskPath)
+		if err != nil {
+			return err
+		}
+		defer pskFile.Close()
+
+		psk, err := pnet.DecodeV1PSK(pskFile)
+		if err != nil {
+			return err
+		}
+
+		opts = append(opts, libp2p.PrivateNetwork(psk))
+		log.Println("private network is enabled")
+	}
+
+	// connection manager
+	connMgr, err := connmgr.NewConnManager(
+		conf.GetInt("relay.conn_lo"),
+		conf.GetInt("relay.conn_hi"),
+		connmgr.WithGracePeriod(time.Duration(conf.GetInt("relay.conn_grace"))*time.Second),
+	)
+	if err != nil {
+		return err
+	}
+	opts = append(opts, libp2p.ConnectionManager(connMgr))
+
+	// create libp2p host
+	h, err := libp2p.New(opts...)
+	if err != nil {
+		return err
+	}
+
+	// save variables to global
+	gHost = h
+
+	log.Println("PeerID:", h.ID())
+	log.Println("Listen Addrs:", h.Addrs())
 
 	return nil
 }
