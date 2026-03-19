94ed1f4e62f077655fac7da9e1af580ad848be97
Leyuan Pan <leyuanpan@gmail.com>
2026-03-18 23:04:30 -0700

    Modify default config file path

--- conf/conf.go
+++ conf/conf.go
@@ -29,7 +29,7 @@ func init() {
 	)
 
 	pflag.BoolVarP(&showVersion, "version", "V", false, "Show version information.")
-	pflag.StringVarP(&confDirPath, "config", "c", "/etc/peanut-relay/relay.yaml", "Config file path.")
+	pflag.StringVarP(&confDirPath, "config", "c", "/srv/peanut-relay/relay.yaml", "Config file path.")
 	pflag.Parse()
 
 	if showVersion {
@@ -45,17 +45,17 @@ func init() {
 
 	// set default values
 	conf.SetDefault("log.enable_console_log", false)
-	conf.SetDefault("log.path", "/var/log/peanut/relay.log")
+	conf.SetDefault("log.path", "/srv/peanut-relay/relay.log")
 	conf.SetDefault("log.max_size", 500)
 	conf.SetDefault("log.max_backups", 3)
 	conf.SetDefault("log.local_time", true)
 	conf.SetDefault("log.compress", true)
 
-	conf.SetDefault("p2p.private_key_path", "/etc/peanut-relay/private-key.b64")
+	conf.SetDefault("p2p.private_key_path", "/srv/peanut-relay/private-key.b64")
 	conf.SetDefault("p2p.pnet_psk_path", "")
 	conf.SetDefault("p2p.listen_multiaddrs", []string{"/ip4/0.0.0.0/udp/19881/quic-v1"})
 	conf.SetDefault("p2p.discovery_multiaddrs", []string{"/dns4/discovery.cpplint.com/udp/19880/quic-v1/p2p/12D3KooWEJifdgZLnVo87Pres6UqT3wZTGm3T6sVPFxnBQdszvEP"})
-	conf.SetDefault("p2p.allowlist_path", "/etc/peanut-relay/allowlist.yaml")
+	conf.SetDefault("p2p.allowlist_path", "/srv/peanut-relay/allowlist.yaml")
 
 	conf.SetDefault("p2p.relay_conn_lo", 4096)
 	conf.SetDefault("p2p.relay_conn_hi", 8192)
